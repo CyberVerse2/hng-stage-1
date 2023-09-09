2502023f8db81200c665fd0499554834a42f8825
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2023-09-09 12:39:20 +0100

    added build script

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.get('/api', (req, res) => {
   const { slack_name, track } = req.query;
   if (!(slack_name && track)) return res.status(400).json('Page not found');
   const response = {
-    slack_name: 'Ejiofor Celestine',
+    slack_name: slack_name,
     current_day: 'Saturday',
     utc_time: new Date.toUTCString(),
-    track: 'backend',
+    track: track,
     github_file_url:
       'https://github.com/CyberVerse2/hng-stage-1/blob/master/index.js',
     github_repo_url: 'https://github.com/CyberVerse2/hng-stage-1',
